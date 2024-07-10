515bfc29454b90453ed46f6c3285fb9fd0b674d5
Kushal Khandhara <kkhandhara@gmail.com>
2024-07-10 14:29:02 +0530

    Jobs Section4 Added

--- my-app/src/pages/Jobs/Jobs.jsx
+++ my-app/src/pages/Jobs/Jobs.jsx
@@ -2,6 +2,7 @@ import "./Jobs.css";
 import JobsHero from "./Section1/JobsHero";
 import JobsCategory from "./Section2/JobsCategory";
 import PopularJobs from "./Section3/PopularJobs";
+import Jobs4 from "./Section4/Jobs4";
 
 export default function Jobs() {
   return (
@@ -9,6 +10,7 @@ export default function Jobs() {
         <JobsHero/>
         <JobsCategory/>
         <PopularJobs/>
+        <Jobs4/>
     </div>
   )
 }
